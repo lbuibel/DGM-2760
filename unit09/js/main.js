ec179697785f7935aad1d4a02e2cf2c2b28b0170
Logan Uibel <lbuibel@gmail.com>
2020-02-13 10:00:02 -0700

    finished unit 9

--- unit09/js/main.js
+++ unit09/js/main.js
@@ -6,10 +6,11 @@ function duplicateMenu() {
     let newList = document.createElement('ul')
 
     topList.forEach(menuItem => {
+        // new list for bottom nav with corresponding elements
         let newUl = document.createElement('ul')
         let newLI = document.createElement('li')
         let newLink = document.createElement('a')
-        
+
         newLink.setAttribute('href', menuItem.getAttribute('href'))
         newLI.appendChild(newLink)
         document.querySelector('#smallNavArea').appendChild(newUl)
