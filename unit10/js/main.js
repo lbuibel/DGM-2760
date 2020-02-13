7dbcf11e26f6a1e81baf5680662491e61c7e2234
Logan Uibel <lbuibel@gmail.com>
2020-02-13 10:44:27 -0700

    setup unit 10

---
+++ unit10/js/main.js
@@ -0,0 +1,7 @@
+// Heading Info
+document.querySelector('header > h1').innerText = "Hotel California"
+document.querySelector('header > h2').innerText = "Check in Anytime You Want"
+
+// Page Title
+document.querySelector('#pageTitle').innerText = "Hotel California"
+
