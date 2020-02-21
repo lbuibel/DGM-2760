d16b5edc0a351f5b9d044d0630713fb5f1e1a2f9
Logan Uibel <lbuibel@gmail.com>
2020-02-21 11:30:33 -0700

    changed styling, displayed hotel name

--- unit10/main.js
+++ unit10/main.js
@@ -19,14 +19,17 @@ let hotelData = {}
 
 getHotelData().then(data => hotelData = data)
 
+// selected individual buttons (didn't use anchor tags)
 document.querySelector("#fourSeasons").addEventListener("click", hotelInfo);
 document.querySelector("#theEmbassy").addEventListener("click", hotelInfo);
 document.querySelector("#theGrand").addEventListener("click", hotelInfo);
 
 function hotelInfo(event) {
     let hotelChoice = hotelData.hotels.find(hotel => {
+      // change ID to lower case - change hotel JSON name to lower case & remove spacing in name
       return event.target.id.toLowerCase() === hotel.name.toLowerCase().replace(/\s/g, '')
     });
+    document.querySelector('#hotelName').textContent = hotelChoice.name
     document.querySelector("#address").textContent = hotelChoice.address
     document.querySelector("#rooms").textContent = hotelChoice.rooms
     document.querySelector("#gym").textContent = hotelChoice.gym
