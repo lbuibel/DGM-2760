8f79cab16ec098f59805b1c851189b4004020e17
Logan Uibel <lbuibel@gmail.com>
2020-04-08 19:04:57 -0600

    got manufacturer select/option working.  Displayed info based on user select

--- final/js/main.js
+++ final/js/main.js
@@ -5,6 +5,7 @@ document.querySelector('header > h1').innerText = "Moto Finder"
 document.querySelector('#pageTitle').innerText = "Select Motorcycle Manufacturer"
 
 const motoContainer = document.querySelector('#moto-section')
+let htmlSection = document.querySelector('#moto-section')
 
 
 
@@ -26,43 +27,36 @@ async function getMotoModels () {
     }
 }
 
+getMotoModels().then(data => motoModels = data)
+
 let motoBrands = {}
 let motoModels = {}
 let brandArray = []
 
+let brandChoice = null
+
 getMotoBrands().then(data => {
     brandArray = data.data
-    var sel = document.querySelector('#dropDown');
-    for(var i = 0; i < brandArray.length; i++) {
-    var opt = document.createElement('option');
-    opt.innerHTML = brandArray[i].name;
-    opt.value = brandArray[i];
-    sel.appendChild(opt);
-}
     motoBrands = data
+
+    sel = document.getElementById('brands')
+    for(var i = 0; i < brandArray.length; i++) {
+        var opt = document.createElement('option')
+        opt.innerHTML = brandArray[i].name
+        opt.value = brandArray[i].name
+        sel.appendChild(opt)
+    }
+    
 })
 
-getMotoModels().then(data => motoModels = data)
+function getMotoInfo() {
+    var x = document.getElementById("brands").selectedIndex;
+    var brandSelect = document.getElementsByTagName("option")[x].value
 
-
-
-
-
-
-// selected individual buttons (didn't use anchor tags)
-document.querySelector("#ducati").addEventListener("click", brandInfo);
-document.querySelector("#bmw").addEventListener("click", brandInfo);
-document.querySelector("#triumph").addEventListener("click", brandInfo);
-
-function brandInfo(event) {
     let brandChoice = motoBrands.data.find(brand => {
-        return event.target.id ===  brand.name.toLowerCase()
+        return brandSelect.toLowerCase() === brand.name.toLowerCase()
     })
     let brandInfo = motoModels.data.filter(id => brandChoice.id === id.brand_id)
-    let brandNames = motoBrands.data.map(i => i.name)
-    console.log(brandChoice)
-    console.log(brandNames)
-    htmlSection = document.querySelector('#moto-section')
     printMotos(brandInfo, brandChoice, htmlSection)
 }
 
