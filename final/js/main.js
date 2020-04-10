375a835ab4694f64e946fa1ab54f0e7916231f29
Logan Uibel <lbuibel@gmail.com>
2020-04-10 14:24:50 -0600

    setup make and model inputs

--- final/js/main.js
+++ final/js/main.js
@@ -2,7 +2,7 @@
 document.querySelector('header > h1').innerText = "Moto Finder"
 // document.querySelector('header > h2').innerText = "Check in Anytime You Like"
 // Page Title
-document.querySelector('#pageTitle').innerText = "Select Motorcycle Manufacturer"
+document.querySelector('#pageTitle').innerText = "Schedule Service"
 
 const motoContainer = document.querySelector('#moto-section')
 let htmlSection = document.querySelector('#moto-section')
@@ -39,15 +39,47 @@ getMotoBrands().then(data => {
     brandArray = data.data
     motoBrands = data
 
-    sel = document.getElementById('brands')
+
+    brandSelect = document.getElementById('brands')
+    modelSelect = document.getElementById('models')
+    modelSelect.disabled = true
+
     for(var i = 0; i < brandArray.length; i++) {
-        var opt = document.createElement('option')
-        opt.innerHTML = brandArray[i].name
-        opt.value = brandArray[i].name
-        sel.appendChild(opt)
+        var brandOption = document.createElement('option')
+        brandOption.innerHTML = brandArray[i].name
+        brandOption.value = brandArray[i].name
+        brandOption.className = "brand-option"
+        brandSelect.appendChild(brandOption)
     }
+    brandSelect.addEventListener('change', () => {
+        modelSelect.disabled = false
+        var x = document.getElementById("brands").selectedIndex;
+        let brandChoice = this.document.getElementsByClassName("brand-option")[x - 1].value
+        console.log(brandChoice)
+        let brandPick = motoBrands.data.find(brand => {
+            return brandChoice.toLowerCase() === brand.name.toLowerCase()
+        })
+        let brandModels = motoModels.data.filter(id => brandPick.id === id.brand_id)
+
+        for(var i = 0; i < brandModels.length; i++) {
+            var modelOption = document.createElement('option')
+            modelOption.innerHTML = brandModels[i].name
+            modelOption.value = brandModels[i].name
+            modelOption.className = "model-option"
+            modelSelect.appendChild(modelOption)
+            }
+            modelSelect.addEventListener('change', () => {
+                var x = document.getElementById("models").selectedIndex;
+                let modelChoice = this.document.getElementsByClassName('model-option')[x - 1].value
+                console.log(modelChoice)
+            })
+        })
     
 })
+
+
+
+
 
 function getMotoInfo() {
     var x = document.getElementById("brands").selectedIndex;
@@ -59,6 +91,8 @@ function getMotoInfo() {
     let brandInfo = motoModels.data.filter(id => brandChoice.id === id.brand_id)
     printMotos(brandInfo, brandChoice, htmlSection)
 }
+
+
 
 function printMotos(array, brand, section) {
     let listCard = document.createElement('div')
